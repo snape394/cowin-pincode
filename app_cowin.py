b6e6159b277189913cf13b5a2dd0f80f5bda6c85
snape394 <oj.bigapple@gmail.com>
2021-05-20 10:10:07 +0530

    Some commit

--- app_cowin.py
+++ app_cowin.py
@@ -24,12 +24,12 @@ def start():
         df = pd.read_csv('pincode_details.csv')
         for index, row in df.iterrows():
             city = str(row['pincode'])
-            geolocator = Nominatim(user_agent="http")
-            location = geolocator.geocode(city)
-            if location:
-                df.loc[index, 'latitude'], df.loc[index, 'longitude'] = location.latitude, location.longitude
-                print(row['officename'], ' : ', (location.latitude, location.longitude))
-            time.sleep(1.0)
+            if row['circlename'] == 'Uttar Pradesh':
+                geolocator = Nominatim(user_agent="http")
+                location = geolocator.geocode(city)
+                if location:
+                    df.loc[index, 'latitude'], df.loc[index, 'longitude'] = location.latitude, location.longitude
+                    print(row['officename'], ' : ', (location.latitude, location.longitude))
         df.to_csv("all-details.csv", index=False)
 
     return 'Completed'
